7397c4f6393d34b9370f0af063f610b276806a5d
Hailemariam Gebrekidan <midextam@gmail.com>
2021-02-26 21:23:58 -0600

    edited

--- w1d5_functions/functions.js
+++ w1d5_functions/functions.js
@@ -22,19 +22,46 @@ let number=prompt("Enter number : ");
 // Question number 2
 // Area of circle
 
-let radius=prompt("Enter the radius of the circle : 10");
-function areaOfCircle(radius=0){
-     let circleArea=(Math.PI*radius*radius)/2;
-    return console.log(circleArea);
+let radius=prompt("Enter the radius of the circle : ");
+let height=prompt("Enter the height of the cylinder : ");
+function areaOfCircle(r=0){
+     let circleArea=(Math.PI*r*r)/2;
+    return circleArea;
 }
-(areaOfCircle(radius));
+  console.log(areaOfCircle(radius));
 
-function volumeOfCylinder(height=0){
-    let cylinderVolume=(areaOfCircle)*height;
-    return console.log(cylinderVolume);
+function volumeOfCylinder(r=0,h=0){
+    let cylinderVolume=areaOfCircle(r)*h;
+    return cylinderVolume;
 }
-let length=prompt("Enter the height of the cylinder : ");
-(volumeOfCylinder(length));
+let result=volumeOfCylinder(radius,height);
+console.log(result);
 
 //Question number 3
+
+let prompt = require("prompt-sync")();
+let sweep = prompt("Enter sweep of the house :");
+let height = prompt("Enter height of the house:");
+height = parseFloat(height);
+let width = prompt("Enter width of the house :");
+width = parseFloat(width);
+let depth = prompt("Enter the depth of the house :");
+depth = parseFloat(depth);
+
+function houseVolume(w, d, h, sw) {
+ return roofVolume(w, sw, d) + livingVolume(w, h, d);
+}
+function livingVolume(w, h, d) {
+ return w * h * d;
+}
+function roofVolume(w, sw, d) {
+ return triangleArea(w, sw) * d;
+}
+function triangleArea(w, sw) {
+ let s = (w + 2 * sw) / 2;
+ let x = (s - w) * (s - sw) * (s - sw);
+ return Math.sqrt(s * x);
+}
+console.log("house volume is :" + houseVolume(width, depth, height, sweep));
+//console.log("house volume is :" + houseVolume());
 
