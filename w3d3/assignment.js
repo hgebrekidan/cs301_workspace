a293fe481e99b02aa209b77f9d869d32cb2e719c
Hailemariam Gebrekidan <midextam@gmail.com>
2021-03-10 21:54:58 -0600

    edited

--- w3d3/assignment.js
+++ w3d3/assignment.js
@@ -1,22 +1,22 @@
 /*eslint-disable*/
 
 function addition(){
-    let num1=document.getElementById("num1");
-    let num2=document.getElementById("num2");
-    let sum=num1+num2;
+    let number1=document.getElementById("num1").value;
+    let number2=document.getElementById("num2").value;
+    let sum=parseInt(number1)+parseInt(number2);
     document.getElementById("result").innerHTML=sum ;
 }
 
 function subtraction(){
-    let num1=document.getElementById("num1");
-    let num2=document.getElementById("num2");
-    let subt=num1-num2;
+    let number1=document.getElementById("num1").value;
+    let number2=document.getElementById("num2").value;
+    let subt=parseInt(number1)-parseInt(number2);
     document.getElementById("result").innerHTML=subt ;
 }
 
 function multiplication(){
-    let num1=document.getElementById("num1");
-    let num2=document.getElementById("num2");
-    let mult=num1*num2;
-    document.getElementById("result").innerHTML=mult ;
+    let number1=document.getElementById("num1").value;
+    let number2=document.getElementById("num2").value;
+    let mult=parseInt(number1)*parseInt(number2);
+    document.getElementById("result").innerHTML=mult;
 }
